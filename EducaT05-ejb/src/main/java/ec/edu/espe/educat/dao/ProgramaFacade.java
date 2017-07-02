c203ce10b905ed7ae17b98a28b6576782d5b12d0
diegosabora7 <diegosabora7@hotmail.com>
2017-07-02 14:36:51 -0500

    Documentacion de la Clase

--- EducaT05-ejb/src/main/java/ec/edu/espe/educat/dao/ProgramaFacade.java
+++ EducaT05-ejb/src/main/java/ec/edu/espe/educat/dao/ProgramaFacade.java
@@ -1,7 +1,15 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+@(#)ProgramaFacade.java     1.0 02/07/2017
+*
+*Copyright 2017 Grupo_05 All rights reserved.
+*Grupo_05 Propietary. Use is subject to license terms
+*/
+/**
+ * Clase que actua como punto de entrada a la Clase Programa 
+ * y hereda los métodos de la misma.
+ * ProgramaFacade es una interfaz unificada sencilla que actua como intermediaria
+ * entre el cliente y la interfaz o grupo de interfaces mas complejas, delegando
+ * las solicitudes del cliente a los objetos del subsistema correspondientes.
  */
 package ec.edu.espe.educat.dao;
 
@@ -11,8 +19,8 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 /**
- *
- * @author Alejandro Torres
+ * @version 1.0
+ * @author Diego Yandun
  */
 @Stateless
 public class ProgramaFacade extends AbstractFacade<Programa> implements ProgramaFacadeLocal {
